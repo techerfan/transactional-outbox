454e9508f9dbf85fe6c28093e64eb0de154407f3
Erfan Derakhshani <techerfan@gmail.com>
2025-03-10 12:54:22 +0000

    Feat: initial implementation of order main cmd

--- cmd/order/main.go
+++ cmd/order/main.go
@@ -1,5 +1,37 @@
 package main
 
+import (
+	"order/adapter/rabbitmq"
+	"order/delivery/grpcserver/shipment"
+	"order/delivery/httpserver"
+	"order/repository/postgres"
+	"order/service/orderservice"
+)
+
 func main() {
 
+	rabbitmqAdapter, err := rabbitmq.New(rabbitmq.Config{
+		Username: "guest",
+		Password: "guest",
+		Host:     "localhost",
+		Port:     5672,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	db := postgres.New(postgres.Config{
+		Host:     "localhost",
+		Port:     5432,
+		Username: "postgres",
+		Password: "postgres",
+		DBName:   "postgres",
+	})
+
+	orderService := orderservice.New(rabbitmqAdapter, db)
+
+	grpcServer := shipment.New(*orderService)
+
+	server := httpserver.New(orderService)
+	server.Start(":8080")
 }
